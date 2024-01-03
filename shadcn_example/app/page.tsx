6ae43226a9707fa179586c5e3aff48445f6a044a
aashiagrawal <aashia2@illinois.edu>
2024-01-03 01:49:01 -0500

    created tailwind example and started shadcn example

---
+++ shadcn_example/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import DashExample from '@/components/dashboard'
+
+export default function Home() {
+  return (
+    <main >
+      <div>
+        <DashExample></DashExample>
+      </div>
+    </main>
+  )
+}
