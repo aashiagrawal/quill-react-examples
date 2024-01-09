a055fd470c02737e49c560821868d0edfdbd8dcd
aashiagrawal <aashia2@illinois.edu>
2024-01-09 11:26:26 -0500

    fixed default page

---
+++ shadcn_example/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import DashExample from '@/quill_components/DashboardExample'
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <DashExample uiname={"default"}></DashExample>
+      </div>
+    </main>
+  )
+}
