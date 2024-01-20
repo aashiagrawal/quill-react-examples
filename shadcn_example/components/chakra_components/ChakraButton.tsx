b5978d33436351981c8cca40aa7d9b2bbeb59b78
aashiagrawal <aashia2@illinois.edu>
2024-01-20 17:13:12 -0600

    small detail fixes

--- shadcn_example/components/chakra_components/ChakraButton.tsx
+++
@@ -1,8 +0,0 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import { ButtonComponentProps } from '@quillsql/react/src/components/UiComponents'
-
-export function ChakraButton({onClick, label, primary}: ButtonComponentProps){
-  return (
-    <Button colorScheme='blue' variant={primary?'solid': 'outline'} onClick={onClick}>{label}</Button>
-  )
-}
